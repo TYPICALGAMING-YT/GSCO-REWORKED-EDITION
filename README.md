aed07fd64b9f7bab7abf395315dc34339538d6c3
TYPICALGAMING-YT <73527546+TYPICALGAMING-YT@users.noreply.github.com>
2020-12-26 15:12:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# GSCO-REWORKED-EDITION
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,50 @@
+var car , wall
+
+var speed , weight
+
+var deformation
+
+function setup() {
+  createCanvas(1500,400);
+  car = createSprite(50, 200, 50, 40);
+  car.shapeColor=255
+
+  wall = createSprite(1200,200,60,height/2)  
+  wall.shapeColor=color(80,80,80)
+
+  speed = random(55,90)
+  weight = random(400,1500)
+
+  car.velocityX = speed
+  
+  deformation = 0.5 * weight * speed * speed / 22500
+  console.log(deformation)
+}
+
+function draw() {
+  background(0);   
+
+    if(wall.x- car.x   <(car.width+wall.width)  /2)
+    {
+      car.velocityX-0;
+      var deformation=0.5 * weight * speed * speed/22500;
+      if(deformation > 180)
+      {
+        car.shapeColor=color(225,0,0);
+      }
+      if(deformation<180 && deformation>100)
+      {
+        car.shapeColor=color(230,230,0);
+
+      }
+
+      if(deformation<100)
+      {
+        car.shapeColor=color(0,255,0);
+      }
+    }
+    
+  drawSprites();
+}
+
+
